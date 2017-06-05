d1a8095b077afb05935adbafae3ee9974ceeb535
Jonathan Lin <jclin2013@gmail.com>
2017-06-04 22:01:02 -0400

    Commit before experimenting with ListView

--- app/containers/Home.js
+++ app/containers/Home.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
-import { ScrollView, View, Text, TextInput, Image, TouchableHighlight, StyleSheet } from 'react-native';
+import { ScrollView, View, Text, TextInput, Image, TouchableHighlight, StyleSheet, ActivityIndicator } from 'react-native';
 import { connect } from 'react-redux';
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.searchPressed = this.searchPressed.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.state = {searchTerms: "", searching: false};
   }
 
   searchPressed() {
-    this.props.fetchImages('yellow flower');
+    this.setState({searching: true});
+    this.props.fetchImages(this.state.searchTerms)
+      .then(() => this.setState({searching: false}));
+  }
+
+  handleChange(text) {
+    this.setState({ searchTerms: text });
   }
 
   render() {
@@ -17,27 +25,67 @@ class Home extends Component {
     let imageList = this.props.images.map(image =>
       <View key={image.id}>
         <Image source={{uri: image.webformatURL}} style={{height: 300}} />
-        <Text>{image.tags}</Text>
       </View>
     )
 
     return (
-      <View style={{marginTop: 20}}>
+      <View style={styles.scene}>
 
-        <View>
-          <TouchableHighlight onPress={ this.searchPressed }>
-            <Text>Fetch Images</Text>
+        <View style={styles.searchSection}>
+          <TextInput
+            style={styles.searchInput}
+            returnKeyType='search'
+            placeholder='Type keywords here...'
+            onChangeText={ this.handleChange }
+            value={ this.state.searchTerms.toLowerCase() }
+          />
+
+          <TouchableHighlight style={styles.searchButton} onPress={ this.searchPressed }>
+            <Text>Search</Text>
           </TouchableHighlight>
         </View>
 
-        <ScrollView>
-          {imageList}
+        {this.state.searching && <ActivityIndicator
+            animating={this.state.searching}
+            style={{height: 600}}
+            size="large"
+          />}
+
+        <ScrollView style={styles.scrollSection}>
+          {!this.state.searching && imageList}
         </ScrollView>
 
       </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+  scene: {
+    flex: 1,
+    marginTop: 20
+  },
+
+  searchSection: {
+    flexDirection: 'row',
+    height: 30,
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    padding: 5
+  },
+
+  searchInput: {
+    flex: 0.85
+  },
+
+  searchButton: {
+    flex: 0.15
+  },
+
+  scrollSection: {
+  }
+
+})
 
 const mapStateToProps = state => {
   return { images: state.images }
--- app/util/api.js
+++ app/util/api.js
@@ -1,3 +1,3 @@
-export const fetchImages = searchTerms => (
-  fetch(`https://pixabay.com/api/?key=5541777-820f16c4e62808ceac76b49c4&q=${encodeURIComponent(searchTerms)}&image_type=photo`)
+export const fetchImages = (searchTerms, pageNum = 1) => (
+  fetch(`https://pixabay.com/api/?key=5541777-820f16c4e62808ceac76b49c4&q=${encodeURIComponent(searchTerms)}&image_type=photo&page=${pageNum}`)
 );
